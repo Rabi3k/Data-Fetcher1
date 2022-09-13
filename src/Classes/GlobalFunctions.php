6b625d1a10f42803f4d846523bca32e861d5fd4e
Rabi3K <Rabih@kbs-leb.com>
2022-09-13 16:17:07 +0200

    Fix design and stuff

---
+++ src/Classes/GlobalFunctions.php
@@ -0,0 +1,16 @@
+<?php 
+namespace Src\Classes;
+
+class GlobalFunctions{
+
+    public static function setResponseHead($httpStatusCode = 521,$httpStatusMsg  = 'Web server is down')
+    {
+        $phpSapiName    = substr(php_sapi_name(), 0, 3);
+        if ($phpSapiName == 'cgi' || $phpSapiName == 'fpm') {
+            header('Status: '.$httpStatusCode.' '.$httpStatusMsg );
+        } else {
+            $protocol = isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0';
+            header($protocol.' '.$httpStatusCode.' '.$httpStatusMsg);
+        }
+    }
+}
