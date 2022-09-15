2f2d1409f30ebce2ae6b6af525178f07aa299568
Rabi3K <Rabih@kbs-leb.com>
2022-09-15 15:14:34 +0200

    minor bug Fix

--- templates/r3k/kds/create-card.php
+++ templates/r3k/kds/create-card.php
@@ -11,63 +11,63 @@ if(!isset($row))
 {
     die();
 }
-$OrderDate = new \DateTime($row["fulfill_at"]??$row["updated_at"]);
-    $OrderDate->setTimezone( new \DateTimeZone($row["restaurant_timezone"]));
+$OrderDate = new \DateTime($row->fulfill_at??$row->updated_at);
+    $OrderDate->setTimezone( new \DateTimeZone($row->restaurant_timezone));
     $jDate=$OrderDate->format('Y/m/d H:i:s');
     $oDate= $OrderDate->format('l-M-y  H:i:s');
 
     
     $todayW = (new \DateTime())->setTimestamp(strtotime("-10 minutes"));
-    $todayW->setTimezone( new \DateTimeZone($row["restaurant_timezone"]));
+    $todayW->setTimezone( new \DateTimeZone($row->restaurant_timezone));
     $today = new \DateTime();
-    $today->setTimezone( new \DateTimeZone($row["restaurant_timezone"]));
+    $today->setTimezone( new \DateTimeZone($row->restaurant_timezone));
     $bgClass='bg-info';
    
 ?>
-<div id='accordion_<?php echo $row["id"] ?>' class='card opacity-90 <?php echo $bgClass ?>'>
-    <!-- <input type='hidden' id='bgClass_<?php echo $row["id"] ?>' value='<?php echo $bgClass ?>'/> -->
-    <input type='hidden' name="OrderDate" id='OrderDate_<?php echo $row["id"] ?>' value='<?php echo $jDate ?>'/>
+<div id='accordion_<?php echo $row->id ?>' class='card opacity-90 <?php echo $bgClass ?>'>
+    <!-- <input type='hidden' id='bgClass_<?php echo $row->id ?>' value='<?php echo $bgClass ?>'/> -->
+    <input type='hidden' name="OrderDate" id='OrderDate_<?php echo $row->id ?>' value='<?php echo $jDate ?>'/>
     <!-- <input type='hidden' id='printValue' value='$printValue'/> -->
-    <button class='btPrint bg-secondary text-light' id='print_<?php echo $row["id"] ?>' onclick='<?php echo "PrintElem(".$row["id"].")" ?>'><i class='fa fa-print' aria-hidden='true'></i></button>
+    <button class='btPrint bg-secondary text-light' id='print_<?php echo $row->id ?>' onclick='<?php echo "PrintElem(".$row->id.")" ?>'><i class='fa fa-print' aria-hidden='true'></i></button>
 
-    <div class='card-header row ' data-toggle='collapse' data-target='#collapse_<?php echo $row["id"] ?>' aria-expanded='true' aria-controls='collapse_<?php echo $row["id"] ?>'>
+    <div class='card-header row ' data-toggle='collapse' data-target='#collapse_<?php echo $row->id ?>' aria-expanded='true' aria-controls='collapse_<?php echo $row->id ?>'>
             <div class='col-6'>
-                <span>Id: <?php echo $row["id"] ?><br/>
-                Name: <?php echo $row["client_first_name"]." ".$row["client_last_name"]; ?></span>
+                <span>Id: <?php echo $row->id ?><br/>
+                Name: <?php echo $row->client_first_name." ".$row->client_last_name; ?></span>
             </div>
             <div class='col-6 text-right'>
-                <p>Shipment: <?php echo $row["type"] ?><br/>
-                Payment: <?php echo $row["payment"] ?></p>
+                <p>Shipment: <?php echo $row->type ?><br/>
+                Payment: <?php echo $row->payment ?></p>
             </div>
             <div class='col-12'>Til: <?php echo $oDate ?></div>
     </div>
-    <div id='collapse_<?php echo $row["id"] ?>' class='collapse card-body text-center' aria-labelledby='headingOne' data-parent='#accordion_<?php echo $row["id"] ?>'>
+    <div id='collapse_<?php echo $row->id ?>' class='collapse card-body text-center' aria-labelledby='headingOne' data-parent='#accordion_<?php echo $row->id ?>'>
         <ul class='list-group text-white'>
-        <?php foreach($row["items"] as $item){ 
-            if($item["type"] === "item")
+        <?php foreach($row->items as $item){ 
+            if($item->type === "item")
             { ?>
                 <li class='list-group-item card-text list-group-item-secondary d-flex justify-content-between align-items-start'>
-                    <div class='ms-2 me-auto fw-bold'><?php echo $item["name"] ?></div>
+                    <div class='ms-2 me-auto fw-bold'><?php echo $item->name ?></div>
                     
-                    <span class='badge bg-secondary text-white rounded-pill'><?php echo $item["quantity"] ?></span>
+                    <span class='badge bg-secondary text-white rounded-pill'><?php echo $item->quantity ?></span>
                 </li>
                 <div class='container'>
                 
                     <ul class='list-group'>
-                    <?php if($item["instructions"])
+                    <?php if($item->instructions)
                     { ?>
                        
                         <li class='list-group-item list-group-item-danger d-flex justify-content-between align-items-start'>
-                            <div class='ms-2 me-auto fw-bold'><?php echo $item["instructions"] ?></div>
+                            <div class='ms-2 me-auto fw-bold'><?php echo $item->instructions ?></div>
                         </li>
                 <?php } 
-                    foreach($item["options"] as $option)
+                    foreach($item->options as $option)
                     {?>
                         
                         <li class='list-group-item list-group-item-primary d-flex justify-content-between align-items-start'>
-                            <div class='ms-2 me-auto fw-bold'><?php echo $option["group_name"] ?></div>
-                            <div class='ms-2 me-auto fw-bold'><?php echo $option["name"] ?></div>
-                            <span class='badge bg-secondary text-white rounded-pill'><?php $option["quantity"] ?></span>
+                            <div class='ms-2 me-auto fw-bold'><?php echo $option->group_name ?></div>
+                            <div class='ms-2 me-auto fw-bold'><?php echo $option->name ?></div>
+                            <span class='badge bg-secondary text-white rounded-pill'><?php $option->quantity ?></span>
                            
                         </li>
                 <?php } ?>
