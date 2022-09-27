a99f2ec141ada70becdec5302c77e6e693a29b78
Rabi3K <Rabih@kbs-leb.com>
2022-09-27 08:43:26 +0200

    Favicon

--- templates/r3k/head.php
+++ templates/r3k/head.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <title><?=$PageTitle?></title>
-<link rel="icon" type="image/x-icon" href="/media/system/favicon.ico">
+<link rel="icon" type="image/x-icon" href="/media/System/favicon.ico">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
