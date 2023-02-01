0309f1885d79855ea38d6fd2baf90abbcbc13494
Rabi3K <Rabih@kbs-leb.com>
2023-02-01 13:44:07 +0100

    Carousel Design
    Signed-off-by: Rabi3K <Rabih@kbs-leb.com>

--- templates/r3k/ods/load-cards.php
+++ templates/r3k/ods/load-cards.php
@@ -27,31 +27,117 @@ $idOrders = array_column($data, 'id');
 
     <?php
     //foreach ($data as $row) {
-        //include "create-card.php";
-    //} ?>
+    //include "create-card.php";
+    //} 
+    ?>
 </div> -->
 <style>
-.card {
-    min-width: 30em;
-    margin-right: 5px;
-}
-.overflow-div-x
-{
-overflow-x: auto !important;
-height: -webkit-fill-available;
-}
-.h-90 {
-    height: 90vh;
-}
+    /* .card {
+        min-width: 30em;
+        margin-right: 5px;
+    }
+
+    .overflow-div-x {
+        overflow-x: auto !important;
+        height: -webkit-fill-available;
+    }
+*/
+    .h-90 {
+        height: 90vh;
+    }
+
+    .carousel-inner {
+        padding: 1em;
+    }
+
+    .card {
+        margin: 0 0.5em;
+        box-shadow: 2px 6px 8px 0 rgba(22, 22, 26, 0.18);
+        border: none;
+    }
+
+    .btn-next,
+    .btn-prev {
+        width: 5vh;
+        height: 5vh;
+        border-radius: 50%;
+        z-index: 0;
+    }
+
+    .controls-container {
+        display: flex;
+        position: absolute;
+        top: 85vh;
+
+        left: 48vw
+    }
+
+    @media (min-width: 1260px) {
+        .carousel-item {
+            margin-right: 0;
+            flex: 0 0 33.333333%;
+            display: block;
+        }
+
+        .carousel-inner {
+            display: flex;
+        }
+    }
+
+    @media (max-width: 1260px) {
+        .carousel-item {
+            margin-right: 0;
+            flex: 0 0 50%;
+            display: block;
+        }
+
+        .carousel-inner {
+            display: flex;
+        }
+
+        .controls-container {
+            top: 80vh;
+        }
+    }
+
+    @media (max-width: 768px) {
+        .carousel-item {
+            margin-right: 0;
+            flex: 0 0 100%;
+            display: block;
+        }
+
+        .carousel-inner {
+            display: flex;
+        }
+        .controls-container {
+            top: 75vh;
+        }
+    }
 </style>
-<div class="container-fluid py-2 h-90">
-    <div class="d-flex flex-row flex-nowrap overflow-div-x" id="orderCards">
-    <?php
-    foreach ($data as $row) {
-        include "create-card.php";
-    } ?>
+<!-- <div class="container-fluid py-2 h-90">
+    <div class="d-flex flex-row flex-nowrap overflow-div-x" id="orderCards"> -->
+<div id="carouselExampleInterval" class="carousel slide h-90" data-ride="carousel">
+    <div class="carousel-inner">
+        <?php
+        foreach ($data as $key => $row) { ?>
+            <div class="carousel-item" data-interval="<?php echo $key ?>">
+                <?php include "create-card.php"; ?>
+            </div>
+        <?php } ?>
     </div>
+    <div class="controls-container"> <a class="btn-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="btn-next" href="#carouselExampleInterval" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+
 </div>
+
 <script>
     $(document).ready(function() {
         $(".btn-branch").click(function() {
@@ -63,6 +149,35 @@ height: -webkit-fill-available;
                 //hide cards
                 $("div[tag=" + tag + "]").hide()
             }
-        })
-    })
+        });
+
+    });
+    var multipleCardCarousel = document.querySelector(
+        "#carouselExampleInterval"
+    );
+
+
+    var carouselWidth = $(".carousel-inner")[0].scrollWidth;
+    var cardWidth = $(".carousel-item").width();
+    var scrollPosition = 0;
+    $("#carouselExampleInterval .btn-next").on("click", function() {
+        if (scrollPosition < carouselWidth - cardWidth * 4) {
+            scrollPosition += cardWidth;
+            $("#carouselExampleInterval .carousel-inner").animate({
+                    scrollLeft: scrollPosition
+                },
+                600
+            );
+        }
+    });
+    $("#carouselExampleInterval .btn-prev").on("click", function() {
+        if (scrollPosition > 0) {
+            scrollPosition -= cardWidth;
+            $("#carouselExampleInterval .carousel-inner").animate({
+                    scrollLeft: scrollPosition
+                },
+                600
+            );
+        }
+    });
 </script>
